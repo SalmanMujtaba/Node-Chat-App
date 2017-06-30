c15d675d2929e0022540ff21e6202c3441652997
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-06-30 11:34:52 -0400

    Part of first commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
-const port = process.enc.PORT || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 app.listen(port,()=>{
