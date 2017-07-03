1dbac99939ca56e9d07fe75b7ed7e11b7a2e98db
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-07-03 16:36:46 -0400

    Add form for chat

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,22 @@ socket.on('disconnect', ()=>{
   console.log('disconnected from server');
 });
 
-socket.on('newMessage', function(message){
-  console.log(message);
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+     from: 'User',
+     text: jQuery('[name=message]').val()
+   }, function () {
+
+   });
+ });
